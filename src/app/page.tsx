baebb0ff56270087c3b8fe991588a4cc4153fc4b
omer-os <omerchetin19@gmail.com>
2024-03-31 02:21:23 +0300

    d

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,21 @@
 import Image from "next/image";
+import { restaurants } from "../../restaurants";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <main >
-      <div className=""></div>
+    <main>
+      <div className="flex flex-col gap-2">
+        {restaurants.map((restaurant) => (
+          <Link
+            href={`/restaurant/${restaurant.subdomain}`}
+            key={restaurant.subdomain}
+            className="flex gap-2 items-center"
+          >
+            {restaurant.name}
+          </Link>
+        ))}
+      </div>
     </main>
   );
 }
