10094a892a0aa166f53c86b8005ccef3014d568b
omer-os <omerchetin19@gmail.com>
2024-03-31 02:20:33 +0300

    Initial commit from Create Next App

---
+++ restaurants.ts
@@ -0,0 +1,22 @@
+export const restaurants = [
+  {
+    name: "McDonald's",
+    subdomain: "mcdonalds",
+  },
+  {
+    name: "Burger King",
+    subdomain: "burger-king",
+  },
+  {
+    name: "KFC",
+    subdomain: "kfc",
+  },
+  {
+    name: "Subway",
+    subdomain: "subway",
+  },
+  {
+    name: "Pizza Hut",
+    subdomain: "pizza-hut",
+  },
+];
